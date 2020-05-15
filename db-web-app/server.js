d93f1ee0d874718fb00f19dee68522c6a8caad65
scorpgyal <julia.litvin.jl@gmail.com>
2020-05-15 16:21:42 +0200

    Update server.js

--- db-web-app/server.js
+++ db-web-app/server.js
@@ -4,6 +4,7 @@
     var express = require('express'); 
     var app = express(); 
     var path = require('path'); 
+    var mysql = require('mysql'); 
 
     // configuration ==================
 
@@ -16,6 +17,33 @@
         res.sendFile('index.html', { root: _dirname+'/dist/db-web-app'});
     }); 
 
+    app.get('/test/', function(req, res)
+    {
+        var connection = mysql.createConnection({
+            host: "195.37.176.178", 
+            port: "20133",
+            user: "Gruppe5", 
+            password: "PlrROASg,'MPyp92yVN/Q00/Y\?8g+1e", 
+            database: "lagom"
+        });
+
+        connection.connect(function(err)
+        {
+            if(err) throw err; 
+            console.log("connected"); 
+
+            connection.log("connected"); 
+
+            connection.query("SELECT * FROM Nutzer", function(err, result)
+            {
+                if(err) throw err; 
+                res.send(result); 
+            }); 
+
+            connection.end(); 
+
+        }); 
+    });
 
 
     // listen (start app with node server.js) =========================
