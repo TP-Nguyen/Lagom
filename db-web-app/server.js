4ad0501fe726869e6a4f89becbbdc9735f4ac08d
scorpgyal <julia.litvin.jl@gmail.com>
2020-05-22 14:39:19 +0200

    server.js gecleaned

--- db-web-app/server.js
+++ db-web-app/server.js
@@ -1,47 +1,46 @@
 // server.js
 
     // set up =========================
-    var express = require('express'); 
-    var app = express(); 
+    var express = require('express');  
     var path = require('path'); 
     var mysql = require('mysql'); 
+    var cors = require('cors');
+    var app = express();
+
+    app.use(cors());
+
 
     // configuration ==================
 
     app.use(express.static(path.join(__dirname, '/src/app'))); 
     // app.use(express.static(path.join(__dirname, '/dist/db-web-app'))); 
 
+    var connection = mysql.createConnection({
+        host: "195.37.176.178", 
+        port: "20133",
+        user: "Gruppe5", 
+        password: "PlrROASg,'MPyp92yVN/Q00/Y\\?8g+1e", 
+        database: "20_Gruppe5_DB"
+    });
+    
     // application ---------------------------------------------------
 
     app.get('/', function(req,res)
     {
         res.sendFile('app.component.html', { root: __dirname + '/src/app'});
-        res.sendFile('app.component.ts', { root: __dirname + '/src/app'});
-        // res.sendFile('index.html', { root: __dirname + '/dist/db-web-app'});
     }); 
     app.get('/main', function(req,res)
     {
         res.sendFile('main.component.html', { root: __dirname + '/src/app'});
-        // res.sendFile('app.component.ts', { root: __dirname + '/src/app'});
-        // res.sendFile('index.html', { root: __dirname + '/dist/db-web-app'});
     }); 
 
+    
     app.get('/test/', function(req, res)
     {
-        var connection = mysql.createConnection({
-            host: "195.37.176.178", 
-            port: "20133",
-            user: "Gruppe5", 
-            password: "PlrROASg,'MPyp92yVN/Q00/Y\\?8g+1e", 
-            database: "20_Gruppe5_DB"
-        });
-
         connection.connect(function(err)
         {
             if(err) throw err; 
-            console.log("connected"); 
-
-            //connection.log("connected"); 
+            console.log("connected");
 
             connection.query("SELECT * FROM 20_Gruppe5_DB.Nutzer", function(err, result)
             {
