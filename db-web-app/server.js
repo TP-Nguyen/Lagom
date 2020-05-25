9abe62db3c3bae711750d4f7dec61138aaa8ce65
larsobist <lars.obist@outlook.de>
2020-05-25 11:46:29 +0200

    Server Update

--- db-web-app/server.js
+++ db-web-app/server.js
@@ -1,79 +1,74 @@
 // server.js
 
-    // set up =========================
-    var express = require('express');  
-    var path = require('path'); 
-    var mysql = require('mysql'); 
-    var cors = require('cors');
-    var app = express();
+const SERVER_PORT = 8080;
 
-    app.use(cors());
+var express = require('express');
+var path = require('path');
+var mysql = require('mysql');
+var cors = require('cors');
+
+// var bodyParser = require('body-parser')
+var app = express();
+var index;
+app.use(cors());
+// app.use(express.bodyParser());
+//  app.use(app.router);
+// parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+// app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, '/dist/db-web-app'))); 
+
+app.get('/', function(req,res)
+{
+    res.sendFile('app.component.html', { root: __dirname + '/src/app'});
+});
+
+app.get('/main', function(req,res)
+{
+    res.sendFile('main.component.html', { root: __dirname + '/src/app'});
+}); 
+
+ //hochschul server
+ const connection = mysql.createConnection({
+    host: "195.37.176.178", 
+    port: "20133",
+    user: "Gruppe5", 
+    password: "PlrROASg,'MPyp92yVN/Q00/Y\\?8g+1e", 
+    database: "20_Gruppe5_DB"
+});
 
 
-    // configuration ==================
 
-    app.use(express.static(path.join(__dirname, '/src/app'))); 
-    // app.use(express.static(path.join(__dirname, '/dist/db-web-app'))); 
 
-    //hochschul server
-    var connection = mysql.createConnection({
-        host: "195.37.176.178", 
-        port: "20133",
-        user: "Gruppe5", 
-        password: "PlrROASg,'MPyp92yVN/Q00/Y\\?8g+1e", 
-        database: "20_Gruppe5_DB"
-    });
 
-    // var server = app.listen(SERVER_PORT, function (){
-    //     let host = server.address().address,
-    //         port = server.address().port;
 
-    //         console.log("Lagom", host, port)
-    // });
 
-    // application ---------------------------------------------------
 
-    app.get('/main', function(req,res)
-    {
-        res.sendFile('main.component.html', { root: __dirname + '/src/app'});
-    }); 
 
     
-    app.get('/test/', function(req, res)
-    {
-        connection.connect(function(err)
-        {
-            // if(err) throw err; 
-            // console.log("connected");
-            connection.query("SELECT * FROM 20_Gruppe5_DB.Nutzer", function(err, result)
-            {
-                if(err) throw err; 
-                res.send(result); 
-            }); 
-            // connection.end(); 
-        }); 
+var server = app.listen(SERVER_PORT, function (){
+    let host = server.address().address,
+        port = server.address().port;
+
+        console.log("App listening at http://%s:%s", host, port)
+});
+
+app.get('/Eintrag', function (req, res) {
+
+    connection.query('SELECT * FROM Eintrag', function (error, results, fields) {
+      if (error) throw error;
+      res.send(results);
+
     });
-    app.get('/Eintrag', function(req, res)
-    {
-        connection.connect(function(err)
-        {
-            connection.query("SELECT * FROM 20_Gruppe5_DB.Eintrag", function(err, result)
-            {
-                if(err) throw err; 
-                res.send(result); 
-            }); 
-            connection.end(); 
-        }); 
-    });
+});
 
-    // app.get('/', function(req,res)
-    // {
-    //     res.sendFile('app.component.html', { root: __dirname + '/src/app'});
-    // });
+app.get('/Kalender', function (req, res) {
 
-    // listen (start app with node server.js) =========================
-    app.listen(8080, function(){
-        console.log("App listening on port 8080");
-    }); 
+    connection.query('SELECT * FROM Kalender', function (error, results, fields) {
+      if (error) throw error;
+      res.send(results);
 
-    // Seite für Leaflet: http://www.liedman.net/leaflet-routing-machine/
+  });
+});
