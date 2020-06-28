aae5d359c14f27de16f51ab2b8f13d5af3bb7632
ezevkyx <ezevkyx@gmx.de>
2020-06-28 18:25:15 +0200

    clean 1

--- db-web-app/src/app/login/login.component.ts
+++ db-web-app/src/app/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { MainService } from '../service/main.service'; 
+import { MainService } from '../service/main.service';
 import { Observable, from } from 'rxjs';
-import { FormBuilder } from '@angular/forms'; 
-import { Nutzer } from '../model/nutzer'; 
+import { FormBuilder } from '@angular/forms';
+import { Nutzer } from '../model/nutzer';
 import { Router } from '@angular/router';
 
 @Component({
@@ -13,80 +13,73 @@ import { Router } from '@angular/router';
 })
 
 export class LoginComponent implements OnInit {
-  title = 'db-web-app';
+  title = 'Lagom';
 
+  //Inhalte initialisieren
   loginTitel = "LOGIN"
-
   nutzerLogin;
-  // @Input() nutzerGefunden : Nutzer;  
   nutzerGefunden: Observable<Nutzer[]>;
   nutzername: Observable<Nutzer>;
-  // nutzerGefunden;//: Nutzer;
   obj;
   workspaceID;
   username = true;
-  //loginForm; 
   nachricht = " ";
 
-  constructor(private mainService: MainService, 
-              private formBuilder: FormBuilder, 
-              private router: Router) {
+  constructor(private mainService: MainService,
+    private formBuilder: FormBuilder,
+    private router: Router) {
     this.nutzerLogin = this.formBuilder.group({
       Nutzername: '',
       Passwort: ''
-    }); 
+    });
   }
 
   ngOnInit(): void {
   }
 
-  changeNutzername(name){
+  changeNutzername(name) {
     this.nutzername = this.mainService.getNutzername(name);
     this.nutzername.subscribe(data => {
-      console.log(data);
       this.username = true;
-      if(data[0]==null) {this.username=false;}
-      if(this.username == false){
-        this.nachricht="Nutzername nicht vorhanden!";
+      if (data[0] == null) { this.username = false; }
+      if (this.username == false) {
+        this.nachricht = "Nutzername nicht vorhanden!";
         console.warn(this.nachricht)
-      }else{
-        this.nachricht="";
+      } else {
+        this.nachricht = "";
       }
     });
   }
-
+  // Nutzerdaten werden überprüft
   submit(nutzerdaten) {
-    const nutzerLogin = new Nutzer(nutzerdaten.Nutzername, nutzerdaten.GanzerName, nutzerdaten.Email, nutzerdaten.Passwort); 
-    this.nutzerLogin.reset(); 
-    console.log("login: " + nutzerLogin.Nutzername )
+    const nutzerLogin = new Nutzer(nutzerdaten.Nutzername, nutzerdaten.GanzerName, nutzerdaten.Email, nutzerdaten.Passwort);
+    this.nutzerLogin.reset();
+    console.log("login: " + nutzerLogin.Nutzername)
     if (nutzerdaten.Nutzername != "" && nutzerdaten.Passwort != "" && this.username) {
-      if(nutzerdaten.Nutzername != null && nutzerdaten.Passwort != null && this.username){
-        this.nutzerGefunden=this.mainService.loginNutzer(nutzerLogin);
+      if (nutzerdaten.Nutzername != null && nutzerdaten.Passwort != null && this.username) {
+        this.nutzerGefunden = this.mainService.loginNutzer(nutzerLogin);
         this.nutzerGefunden.subscribe(data => {
-          console.log(data);
           this.obj = data[0];
-          if(this.obj =="404"){
-            console.log(this.obj);
-            console.log("fail");   
-            this.passwortError();        
-          }else{
-            console.log(data[0].NutzerID);
-            console.log("erfolgreich");
+          if (this.obj == "404") {
+            this.passwortError();
+          } else {
             this.workspaceID = this.mainService.getWorkspaceID(data[0].NutzerID);
-            this.workspaceID.subscribe(data => {console.log(data);
-            this.router.navigate(['/main/' + data[0].WorkspaceID ]);});
+            this.workspaceID.subscribe(data => {
+              this.router.navigate(['/main/' + data[0].WorkspaceID]);
+            });
           }
-        }); 
-      }else{
+        });
+      } else {
         console.log("Daten unvollständig")
         this.showError();
       }
-    }else{
+    } else {
       console.log("Daten unvollständig")
       this.showError();
     }
   }
 
+  // Exceptions werden geworfen
   showError() {
     this.nachricht = "Alle Felder müssen ausgefüllt werden!";
     console.warn(this.nachricht);
--- db-web-app/src/app/main/main.component.ts
+++ db-web-app/src/app/main/main.component.ts
@@ -2,10 +2,10 @@ import { MainService } from '../service/main.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
-import { Eintrag} from '../model/eintrag';
-import { Ziel} from '../model/ziel';
-import { Todo} from '../model/todo';
-import { Erinnerung} from '../model/erinnerung';
+import { Eintrag } from '../model/eintrag';
+import { Ziel } from '../model/ziel';
+import { Todo } from '../model/todo';
+import { Erinnerung } from '../model/erinnerung';
 import { Galerie } from '../model/galerie';
 import { Motivation } from '../model/motivation';
 import { Nutzer } from '../model/nutzer';
@@ -18,23 +18,22 @@ import * as io from 'socket.io-client';
 
 const SOCKET_ENDPOINT = 'localhost:3000';
 @Component({
-  selector: 'app-root', 
+  selector: 'app-root',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 
 export class MainComponent implements OnInit {
   socket;
-  public month: number = new Date().getMonth();
-  public fullYear: number = new Date().getFullYear();
-  public dateValue: Date = new Date(this.fullYear, this.month , 11);
-  public minDate: Date = new Date(this.fullYear, this.month , 9);
-  public maxDate: Date = new Date(this.fullYear, this.month, 15);
+
+
   constructor(private mainService: MainService,
     private router: Router,
     private readonly http: HttpClient,
     private route: ActivatedRoute) { }
 
+  title = 'Lagom';
+  //Inhalte initialisieren
   ziele: Observable<Ziel[]>;
   todos: Observable<Todo[]>;
   erinnerungen: Observable<Erinnerung[]>;
@@ -43,181 +42,87 @@ export class MainComponent implements OnInit {
   nutzer: Observable<Nutzer[]>;
   tagebuch: Observable<Tagebuch[]>;
   kalender: Observable<Kalender[]>;
-  nutzerdaten:Observable<any>;
+  nutzerdaten: Observable<any>;
   WorkspaceID = +this.route.snapshot.paramMap.get('WorkspaceID');
+
+  //Elemente für Kalendar
+  public month: number = new Date().getMonth();
+  public fullYear: number = new Date().getFullYear();
+  public dateValue: Date = new Date(this.fullYear, this.month, 11);
+  public minDate: Date = new Date(this.fullYear, this.month, 9);
+  public maxDate: Date = new Date(this.fullYear, this.month, 15);
 
   ngOnInit() {
     this.setupSocketConnection();
     this.getAllData();
   }
-
+  // Socket
   setupSocketConnection() {
     this.socket = io(SOCKET_ENDPOINT);
     this.socket.on('my broadcast', (data: string) => {
       this.getAllData();
     });
- }
-  
-  private getAllData(): void{
+  }
+  // alle Daten werden aus der Datenbank geholt
+  private getAllData(): void {
     this.nutzerdaten = this.mainService.getNutzerdaten(this.WorkspaceID);
-    this.nutzerdaten.subscribe(data => {});
+    this.nutzerdaten.subscribe(data => { });
 
     this.todos = this.mainService.getTodos(this.WorkspaceID);
-    this.todos.subscribe(data => {console.log(data)});
+    this.todos.subscribe(data => { });
 
     this.ziele = this.mainService.getZiele(this.WorkspaceID);
-    this.ziele.subscribe(data => {});
+    this.ziele.subscribe(data => { });
 
     this.erinnerungen = this.mainService.getErinnerungen(this.WorkspaceID);
-    this.erinnerungen.subscribe(data => {});
+    this.erinnerungen.subscribe(data => { });
 
     this.galerien = this.mainService.getGalerien(this.WorkspaceID);
-    this.galerien.subscribe(data => {});
+    this.galerien.subscribe(data => { });
 
     this.motivationen = this.mainService.getMotivationen(this.WorkspaceID);
-    this.motivationen.subscribe(data => {});
+    this.motivationen.subscribe(data => { });
 
     this.tagebuch = this.mainService.getTagebuch(this.WorkspaceID);
-    this.tagebuch.subscribe(data => {});
+    this.tagebuch.subscribe(data => { });
 
     this.kalender = this.mainService.getKalender(this.WorkspaceID);
-    this.kalender.subscribe(data => {});
+    this.kalender.subscribe(data => { });
+  }
+  // Todo eintrag als erledigt speichern
+  public erledigt(todoEintrag: Todo): void {
+    todoEintrag.Erledigt = !todoEintrag.Erledigt;
+    this.mainService.updateToDo(todoEintrag).subscribe();
+    this.socket.emit('refresh', 'refresh Page');
+  }
+  // loeschen von Eintraegen
+  public deleteZiel(zielEintrag: Eintrag): void {
+    this.mainService.deleteZielEintrag(zielEintrag).subscribe();
+    this.socket.emit('refresh', 'refresh Page');
   }
 
-  public erledigt(todoEintrag: Todo) : void{
-    todoEintrag.Erledigt = !todoEintrag.Erledigt; 
-    this.mainService.updateToDo(todoEintrag).subscribe(); 
-    this.socket.emit('refresh','refresh Page');
-    console.log(todoEintrag.Erledigt); 
+  public deleteKalender(kalenderEintrag: Eintrag): void {
+    this.mainService.deleteKalenderEintrag(kalenderEintrag).subscribe();
+    this.socket.emit('refresh', 'refresh Page');
   }
 
-  public deleteZiel(zielEintrag: Eintrag): void{
-    this.mainService.deleteZielEintrag(zielEintrag).subscribe(); 
-    this.socket.emit('refresh','refresh Page');
+  public deleteTagebuch(tagebuchEintrag: Eintrag): void {
+    this.mainService.deleteTagebuchEintrag(tagebuchEintrag).subscribe();
+    this.socket.emit('refresh', 'refresh Page');
   }
 
-  public deleteKalender(kalenderEintrag: Eintrag): void{
-    this.mainService.deleteKalenderEintrag(kalenderEintrag).subscribe(); 
-    this.socket.emit('refresh','refresh Page');
-  }
-
-  public deleteTagebuch(tagebuchEintrag: Eintrag): void{
-    this.mainService.deleteTagebuchEintrag(tagebuchEintrag).subscribe(); 
-    this.socket.emit('refresh','refresh Page');
-  }
-  
-  public deleteToDo(todoEintrag: Eintrag): void{  
+  public deleteToDo(todoEintrag: Eintrag): void {
     this.mainService.deleteToDoEintrag(todoEintrag).subscribe();
-    this.socket.emit('refresh','refresh Page');
-    
+    this.socket.emit('refresh', 'refresh Page');
   }
 
-  public deleteErinnerung(erinnerungEintrag: Eintrag): void{
-    this.mainService.deleteErinnerungEintrag(erinnerungEintrag).subscribe(); 
-    this.socket.emit('refresh','refresh Page');
+  public deleteErinnerung(erinnerungEintrag: Eintrag): void {
+    this.mainService.deleteErinnerungEintrag(erinnerungEintrag).subscribe();
+    this.socket.emit('refresh', 'refresh Page');
   }
-
-  public eintragErstellen(){
+  // Verlinkung zur Eintrag erstellen Seite
+  public eintragErstellen() {
     this.router.navigate(["eintragErstellen/" + this.WorkspaceID])
   }
-
-  title = 'db-web-app';
-  // month = "Juli";
-  days = [
-    {
-      number: "1"
-    },
-    {
-      number: "2"
-    },
-    {
-      number: "3"
-    },
-    {
-      number: "4"
-    },
-    {
-      number: "5"
-    },
-    {
-      number: "6"
-    },
-    {
-      number: "7"
-    },
-    {
-      number: "8"
-    },
-    {
-      number: "9"
-    },
-    {
-      number: "10"
-    },
-    {
-      number: "11"
-    },
-    {
-      number: "12"
-    },
-    {
-      number: "13"
-    },
-    {
-      number: "14"
-    },
-    {
-      number: "15"
-    },
-    {
-      number: "16"
-    },
-    {
-      number: "17"
-    },
-    {
-      number: "18"
-    },
-    {
-      number: "19"
-    },
-    {
-      number: "20"
-    },
-    {
-      number: "21"
-    },
-    {
-      number: "22"
-    },
-    {
-      number: "23"
-    },
-    {
-      number: "24"
-    },
-    {
-      number: "25"
-    },
-    {
-      number: "26"
-    },
-    {
-      number: "27"
-    },
-    {
-      number: "28"
-    },
-    {
-      number: "29"
-    },
-    {
-      number: "30"
-    },
-    {
-      number: "31"
-    }
-    
-  ];
 
 }
